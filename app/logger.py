ad98332f3f9c1db343b76c3e0adc640b9b483b74
GermanMT <germanoctako@gmail.com>
2025-10-29 18:20:07 +0100

    feat: improve logger class

--- app/logger.py
+++ app/logger.py
@@ -1,16 +1,48 @@
-from logging import DEBUG, Formatter, getLogger
+from logging import INFO, Formatter, getLogger
 from logging.handlers import RotatingFileHandler
+from pathlib import Path
+from typing import Any
 
-uvicorn_access = getLogger("uvicorn.access")
-uvicorn_access.disabled = True
 
-logger = getLogger("uvicorn")
-logger.setLevel(DEBUG)
+class LoggerManager:
+    def __init__(
+        self,
+        log_file: str = "errors.log",
+        max_bytes: int = 5 * 1024 * 1024,
+        backup_count: int = 5,
+    ) -> None:
+        self.logger = getLogger("securechain")
+        self.logger.setLevel(INFO)
+        self.logger.propagate = False
 
-formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        log_path = Path(log_file)
+        log_path.parent.mkdir(parents=True, exist_ok=True)
 
-rotating_handler = RotatingFileHandler("logs.log", maxBytes=5 * 1024 * 1024, backupCount=3)
-rotating_handler.setLevel(DEBUG)
-rotating_handler.setFormatter(formatter)
+        file_handler = RotatingFileHandler(
+            log_file, maxBytes=max_bytes, backupCount=backup_count, encoding="utf-8"
+        )
+        formatter = Formatter(
+            "%(asctime)s - %(levelname)s - %(name)s - %(filename)s:%(lineno)d - %(message)s"
+        )
+        file_handler.setFormatter(formatter)
 
-logger.addHandler(rotating_handler)
+        if not self.logger.handlers:
+            self.logger.addHandler(file_handler)
+
+    def info(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        self.logger.info(msg, *args, **kwargs)
+
+    def warning(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        self.logger.warning(msg, *args, **kwargs)
+
+    def error(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        self.logger.error(msg, *args, **kwargs)
+
+    def exception(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        self.logger.exception(msg, *args, **kwargs)
+
+    def debug(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        self.logger.debug(msg, *args, **kwargs)
+
+
+logger: LoggerManager = LoggerManager("logs/errors.log")
